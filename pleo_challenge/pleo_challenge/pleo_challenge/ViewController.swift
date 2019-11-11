d0c8f67b653a8557dfe0797627a3d71789482bdf
Martin Wiingaard <martin.wiin@gmail.com>
2019-11-11 13:40:41 +0100

    Initialize xcode project.

---
+++ pleo_challenge/pleo_challenge/pleo_challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pleo_challenge
+//
+//  Created by Martin Wiingaard on 11/11/2019.
+//  Copyright © 2019 Fiks IVS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGreen
+    }
+
+}
+
